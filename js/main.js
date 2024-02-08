69455b57f8e4e9699dbb5d94621c71ed1bc67208
denys-lytvyn <79865197+denys-lytvyn@users.noreply.github.com>
2024-02-08 09:36:06 +0100

    Project added

---
+++ js/main.js
@@ -0,0 +1,25 @@
+/* Nav icon */
+const navBtn = document.querySelector('.nav-icon-btn');
+const navIcon = document.querySelector('.nav-icon');
+const nav = document.querySelector('.header__top-row');
+
+navBtn.onclick = function () {
+    navIcon.classList.toggle('nav-icon--active');
+    nav.classList.toggle('header__top-row--mobile');
+    document.body.classList.toggle('no-scroll');
+}
+
+/* Phone Mask */
+mask('[data-tel-input]');
+
+
+const phoneInputs = document.querySelectorAll('[data-tel-input]');
+phoneInputs.forEach((input)=>{
+	input.addEventListener('input', ()=>{
+		if (input.value == '+') input.value = '';
+	})
+	input.addEventListener('blur', ()=>{
+		if (input.value == '+') input.value = '';
+	})
+});
+
